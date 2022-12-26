27e77c3c1fe2687aafccf89d5e67ebd2907dbd89
sky <sky@192.168.200.104>
2022-12-26 21:41:59 +0900

    coding

---
+++ src/coding/ConditionDemo3.java
@@ -0,0 +1,12 @@
+package coding;
+
+public class ConditionDemo3 {
+
+	public static void main(String[] args) {
+		if(false) 
+			System.out.println(1);
+		else 
+			System.out.println(2);
+		
+	}
+}
